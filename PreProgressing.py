786628a456d20f26d63a7d0979bbb9226742eb84
1chooo <hugo970217@gmail.com>
2022-06-25 09:48:48 +0800

    pre-progressing

--- PreProgressing.py
+++ PreProgressing.py
@@ -1,2 +1,12 @@
 import os
+import glob
+import pandas as pd
 
+path_domestic = os.path.abspath(os.getcwd()) + '/data'
+root = "./data"
+data = glob.glob(os.path.join(root, "*.csv"))
+
+
+dataFrame = pd.concat((pd.read_csv(file) for file in data))
+
+dataFrame.to_csv("./20years_data.csv", encoding = "utf_8_sig", index = False) 
