206749fff1966ae8e605178dcd75161059686c28
1chooo <hugo970217@gmail.com>
2022-06-25 10:07:10 +0800

    pre-progressing

--- PreProgressing.py
+++ PreProgressing.py
@@ -2,11 +2,13 @@ import os
 import glob
 import pandas as pd
 
-path_domestic = os.path.abspath(os.getcwd()) + '/data'
-root = "./data"
+
+# Get the root from the computer. Then read the file.
+pathDomestic = os.path.abspath(os.getcwd()) + "/data"
+root = pathDomestic
 data = glob.glob(os.path.join(root, "*.csv"))
 
 
+# Integrate all file in one csv file.
 dataFrame = pd.concat((pd.read_csv(file) for file in data))
-
 dataFrame.to_csv("./20years_data.csv", encoding = "utf_8_sig", index = False) 
